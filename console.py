09708f0aff7e7ac1f16952d139afb200e6a74513
Crysaliux <114651917+Crysaliux@users.noreply.github.com>
2022-10-10 21:35:13 +0400

    Create console.py

---
+++ console.py
@@ -0,0 +1,12 @@
+import time
+
+def sys_start():
+    print("system started...")
+    time.sleep(1)
+    print("loaded main modules...")
+    time.sleep(1)
+    print("loaded data_base...")
+    time.sleep(1)
+    print("loaded language modules...")
+    time.sleep(1)
+    print("system ONLINE")
